06c7b6ba75638a85f8d8f7a03a90721e0fcaa55d
beylsp <beylsp@yahoo.com>
2015-12-07 16:13:16 +0000

    added correct dependencies between requirement files

--- collectr/__init__.py
+++ collectr/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from opbeat.contrib.flask import Opbeat
 
 app = Flask(__name__)
+opbeat = Opbeat(
+    app,
+    organization_id='c6b1b6f06dab409db01224faa85c9887',
+    app_id='209b15af04',
+    secret_token='b5b3969552cbaf97b75e94014bff7108d8c0be6b'
+)
 
 from collectr import views  # noqa
