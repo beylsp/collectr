bea813e326b9c339ee75f2d35b30d932fb021456
beylsp <beylsp@yahoo.com>
2015-12-08 22:28:34 +0000

    fixed import statement for py3x

--- collectr/__init__.py
+++ collectr/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from opbeat.contrib.flask import Opbeat
 from config import config
-from views.products import products
+from collectr.views.products import products
 
 opbeat = Opbeat()
 
