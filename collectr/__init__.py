00802821d81d30dfe72f1ea80ac0481d81987085
beylsp <beylsp@yahoo.com>
2015-12-08 22:56:49 +0000

    PEP257: added missing docstrings

--- collectr/__init__.py
+++ collectr/__init__.py
@@ -1,3 +1,4 @@
+"""The application module, containing the application factory function."""
 from flask import Flask
 from opbeat.contrib.flask import Opbeat
 from config import config
@@ -7,6 +8,11 @@ opbeat = Opbeat()
 
 
 def create_app(config_name):
+    """An application factory, as explained here:
+    http://flask.pocoo.org/docs/patterns/appfactories/.
+
+    :param config_object: The configuration to use.
+    """
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
