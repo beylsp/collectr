85fe374f3a221bff390680b2dffc9d70286538d1
beylsp <beylsp@yahoo.com>
2015-12-09 20:07:05 +0000

    added docstring

--- collectr/__init__.py
+++ collectr/__init__.py
@@ -11,6 +11,9 @@ def create_app(config_name):
     """An application factory, as explained here:
     http://flask.pocoo.org/docs/patterns/appfactories/.
 
+    Opbeat application monitoring is only activated for production and
+    development configurations.
+
     :param config_name: The configuration to use.
     """
     app = Flask(__name__)
