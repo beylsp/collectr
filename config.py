1ca93dec168100ab28dd4a0eda4c7efb5a60c680
beylsp <beylsp@yahoo.com>
2015-12-09 20:03:24 +0000

    PEP257: added missing docstrings

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
+"""Application configuration module."""
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    """Parent configuration."""
     DEBUG = False
     TESTING = False
 
 
 class DevelopmentConfig(Config):
+    """Development configuraton."""
     DEBUG = True
 
 
 class TestingConfig(Config):
+    """Test configuration."""
     TESTING = True
 
 
