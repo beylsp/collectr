64f10813e9247bc1638503884f2949084e47ccfa
Patrik Beyls <beylsp@yahoo>
2018-02-08 22:14:42 +0100

    add scheduled job to redis queue

--- collectr/crawler/scheduler.py
+++ collectr/crawler/scheduler.py
@@ -1,9 +1,15 @@
 import scrapy
+from rq import Queue
+from worker import conn
 from scrapy.crawler import CrawlerProcess
 from crawler.spiders.sm import SparkModelSpider
 
+
+q = Queue(connection=conn)
+
 process = CrawlerProcess()
+process.crawl(SparkModelSpider)
+
 
 if __name__ == '__main__':
-    process.crawl(SparkModelSpider)
-    process.start()
+    result = q.enqueue(process.start)
